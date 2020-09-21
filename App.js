de7427532538e6ee5ecf7dde7165a7b3f4e97063
gpablosl <gpablosleon15@gmail.com>
2020-09-20 21:21:37 -0700

    first commit/react native/hello world

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hola mundo</Text>
+      <Text>tarea hecha el 20/9/2020</Text>
       <StatusBar style="auto" />
     </View>
   );
